dab6ba25a7551316ae071da12a754180598bc6a3
DavidSagan <david.sagan@gmail.com>
2023-06-15 17:34:54 -0400

    Now can parse a simple lat.

--- src/struct_def.jl
+++ src/struct_def.jl
@@ -1,6 +1,52 @@
 using OffsetArrays
 using PyFormattedStrings
 
+#-------------------------------------------------------------------------------------
+
+macro insert_standard_LatEle_fields()
+  return esc(:( name::String; length::Float64 ))
+end
+  ## @insert_standard_LatEle_fields
+
+
+abstract type BeamLineComponent end
+
+"Abstract Lat element from which all elements inherit"
+abstract type LatEle <: BeamLineComponent end
+
+"General thick multipole that is inherited by quadrupoles, sextupoles, etc."
+abstract type ThickMultipole <: LatEle end
+
+"Bend lat element. Equivalent to SBend in Bmad."
+mutable struct Bend <: LatEle
+  name::String
+  param::Dict{String,Any}
+end
+
+"Drift lat element"
+mutable struct Drift <: LatEle
+  name::String
+  param::Dict{String,Any}
+end
+
+"Quadrupole lat element"
+mutable struct Quadrupole <: ThickMultipole
+  name::String
+  param::Dict{String,Any}
+end
+
+"Marker lat element"
+mutable struct Marker <: LatEle
+  name::String
+  param::Dict{String,Any}
+end
+
+beginning_Latele = Marker("beginning", Dict{String,Any}())
+end_Latele       = Marker("end", Dict{String,Any}())
+
+#-------------------------------------------------------------------------------------
+"LatEle parameters"
+
 mutable struct FloorPosition
   r::Vector{Float64}       # (x,y,z) in Global coords
   q::Vector{Float64}       # Quaternion orientation
@@ -14,156 +60,120 @@ mutable struct MultipoleArray
 end
 
 mutable struct LordSlave
-  lord::Union{PhysicalEle,Nothing}
-  slave::Union{Vector{PhysicalEle},Nothing}
-  control_lord::Union{Vector{PhysicalEle},Nothing}
+  lord::Union{LatEle,Nothing}
+  slave::Union{Vector{LatEle},Nothing}
+  control_lord::Union{Vector{LatEle},Nothing}
 end
 
 #-------------------------------------------------------------------------------------
+"Lattice"
 
-macro insert_standard_PhysicalEle_fields()
-  return esc(:( name::String; length::Float64 ))
-end
-  ## @insert_standard_PhysicalEle_fields
+abstract type AbstractLat end
+abstract type AbstractBranch end
 
-
-"Abstract physical element from which all elements inherit"
-abstract type PhysicalEle end
-
-"General thick multipole that is inherited by quadrupoles, sextupoles, etc."
-abstract type ThickMultipole <: PhysicalEle
-end
-
-"Bend lattice element. Equivalent to SBend in Bmad."
-mutable struct Bend <: PhysicalEle
-  name::String
-  lord_slave::Union{LordSlave,Nothing}
-  param::Dict{String,Any}
-end
-
-"Drift lattice element"
-mutable struct Drift <: PhysicalEle
-  name::String
-  lord_slave::Union{LordSlave,Nothing}
-  param::Dict{String,Any}
-end
-
-"Quadrupole lattice element"
-mutable struct Quadrupole <: ThickMultipole
-  name::String
-  lord_slave::Union{LordSlave,Nothing}
-  param::Dict{String,Any}
-end
-
-"Marker lattice element"
-mutable struct Marker <: PhysicalEle
-  name::String
-  lord_slave::Union{LordSlave,Nothing}
-  param::Dict{String,Any}
-end
-
-beginning_physicalele = Marker('beginning', nothing, Dict{String,Any}())
-end_physicalele       = Marker('end', nothing, Dict{String,Any}())
-
-#-------------------------------------------------------------------------------------
-
-abstract type AbstractLattice
-abstract type AbstractBranch
-
-mutable struct LatEle
-  name::String
-  physical_ele::PhysicalEle
-  branch::Union{AbstractBranch,Nothing} 
-  ix_ele::Int
-  param::Dict{String,Any}  # Orientation, floor, floor_end => next ele
-end
-
-LatEle(ele::PhysicalEle, branch::AbstractBranch, ix_ele::Int) = LatEle(ele.name, ele, branch, ix_ele, nothing)
+LatEle(ele::LatEle, branch::AbstractBranch, ix_ele::Int) = LatEle(ele.name, ele, branch, ix_ele, nothing)
 
 mutable struct LatBranch
   name::String
   ele::Vector{LatEle}
-  lattice::Union{AbstractLattice, Nothing}      # Pointer to lattice containing branch
-  ix_branch::Int    
-  from_ele::Union{LatEle, Nothing}  # Creating fork element which forks to this branch.
-  to_ele::Union{LatEle, Nothing}    # Element in this branch that creating fork element forks to.
+  param::Dict{String,Any}
+end
 
-mutable struct Lattice <: AbstractLattice
+mutable struct Lat <: AbstractLat
+  name::String
   branch::Vector{LatBranch}
 end
 
-function show_branch_layout(branch)
-  print ("Branch: $branch.name")
-  for ele in branch.ele
-  
+function show_branch(branch::LatBranch)
+  print(f"{get(branch.param, \"ix_branch\", \"\")} Branch: {branch.name}")
+  n = maximum([6, maximum([length(e.name) for e in branch.ele])])
+  for (ix, ele) in enumerate(branch.ele)
+    print(f"\n  {ix:5i}  {rpad(ele.name, n)}")
+  end
+  return nothing
 end
 
-
-function show_lattice_layout
-
+function show_lat(lat::Lat)
+  print(f"Lat: {lat.name}")
+  for branch in lat.branch
+    print("\n")
+    show_branch(branch)
+  end
+  return nothing
 end
+
+#Base.show(io::IO, lb::LatBranch) = print(io, "Hi!")
+
 
 #-------------------------------------------------------------------------------------
 
-abstract type BeamLineItem
-
-mutable struct BeamLineEle <:BeamLineItem
-  PhysicalEle::PhysicalEle
-  forward_orientation::Bool
-end
+BeamLineItem = Union{BeamLineComponent, Vector{BeamLineComponent}}
 
 "A simple beam line."
-mutable struct BeamLine <: BeamLineItem
-  line::Vector{Union{BeamLineItem,Vector{BeamLineItem}}}
+mutable struct BeamLine <: BeamLineComponent
+  name::String
+  line::Vector{BeamLineItem}
   multipass::Bool
+  orientation::Int
 end
 
-#-------------------------------------------------------------------------------------
-"Functions to construct a lattice."
+#Base.show(io::IO, lb::BeamLine) = print(io, "Hi!")
 
-function ele_def(type::Type{T}, name::String; kwargs...) where T <: PhysicalEle
+#-------------------------------------------------------------------------------------
+"Functions to construct a lat."
+
+function latele(type::Type{T}, name::String; kwargs...) where T <: LatEle
   # kwargs is a named tuple with Symbol keys. Want keys to be Strings.
   return type(name, Dict{String,Any}(string(k)=>v for (k,v) in kwargs))
 end
 
-PhysicalEle_to_beamlineele(PhysicalEle::T) where T <: PhysicalEle
-  return BeamLineEle(PhysicalEle, true)
+function beamline(name::String, line_in::Vector{T}; multipass::Bool = false, orientation = +1) where T <: BeamLineItem
+  return BeamLine(name, line_in, multipass, orientation)
 end
 
-function beamline_def(line_in::Vector{Union{PhysicalEle,BeamLine}}; multipass::Bool = false)
-  return BeamLine(PhysicalEle_to_beamlineele.(line_in), mulitpass)
-end
-
-function push_beamline_item (lat::Lattice, branch::LatBranch, item::Union{BeamLineItem,Vector{BeamLineItem}})
-  if isa(item, BeamLineEle)
-    push!(branch.ele, LatEle(item, branch, size(branch.ele)+1)); end
-  elseif isa(item, Vector{BeamLineEle})
-    for subitem in item; push!(branch.ele, LatEle(subitem, branch, size(branch.ele)+1)); end
-  elseif isa(item, BeamLine) 
-    add_to_latbranch(lat, branch, item); end
-  elseif isa(item, Vector{BeamLine})
-    for subitem in item; add_to_latbranch(lat, branch, subitem); end
-  end
-
+function latele_to_branch!(branch, latele)
+  push!(branch.ele, deepcopy(latele))
+  ele = branch.ele[end]
+  ele.param["ix_ele"] = length(branch.ele)
   return nothing
 end
 
-function add_to_latbranch (lat::Lattice, branch::LatBranch, beamline::BeamLine)
-  branch = lat.branch[end]
-  push!(branch.ele, LatEle(deepcopy(beginning_physicalele), branch, 1))
-  for item in beamline; push_beamline_item (lat, branch, item); end
-end
-
-function new_latbranch(lat::Lattice, beamline::BeamLine)
-  push!(lat.branch, LatBranch(Vector{LatEle}(), lat, size(lat.branch)+1, nothing, nothing)
-  add_to_latbranch(lat, lat.branch[end], beamline)
-  push!(branch.ele, LatEle(deepcopy(end_physicalele), branch, size(branch.ele)+1))
-end
-
-function expand_beamline(root_line::Union{BeamLine,Vector{BeamLine}})
-  if isa(root_line, BeamLine)
-    new_latbranch(lat, root_line)
-  else
-    for rline in root_line; new_latbranch(lat, rline); end
+function beamline_item_to_branch!(branch::LatBranch, item::BeamLineItem)
+  if isa(item, LatEle)
+    latele_to_branch!(branch, item)
+  elseif isa(item, Vector{LatEle})
+    for subitem in item; latele_to_branch!(branch, subitem); end
+  elseif isa(item, BeamLine) 
+    add_to_latbranch!(branch, item)
+  elseif isa(item, Vector{BeamLine})
+    for subitem in item; add_to_latbranch!(branch, subitem); end
   end
+  return nothing
+end
+
+function add_to_latbranch!(branch::LatBranch, beamline::BeamLine)
+  for item in beamline.line; beamline_item_to_branch!(branch, item); end
+  return nothing
+end
+
+function new_latbranch!(lat::Lat, beamline::BeamLine)
+  push!(lat.branch, LatBranch(beamline.name, Vector{LatEle}(), 
+                                              Dict{String,Any}("lat" => lat, "ix_branch" => length(lat.branch)+1)))
+  branch = lat.branch[end]
+  latele_to_branch!(branch, beginning_Latele)
+  add_to_latbranch!(branch, beamline)
+  latele_to_branch!(branch, end_Latele)
+  return nothing
+end
+
+function make_lat(root_line::Union{BeamLine,Vector{BeamLine},Nothing} = nothing, name::String = "")
+  lat = Lat(name, Vector{LatBranch}())
+  if root_line == nothing; root_line = root_beamline end
+  if isa(root_line, BeamLine)
+    new_latbranch!(lat, root_line)
+  else
+    for rline in root_line; new_latbranch!(lat, rline); end
+  end
+  if lat.name == ""; lat.name = lat.branch[1].name; end
+  return lat
 end
